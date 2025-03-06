a6d839c4d09c19b5bb3b7be94de8303c502cd21b
Thiago Adam Camargo <thiagohsm2009@hotmail.com>
2025-03-05 21:06:38 -0300

    First commit

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_vpc" "vpc_workspace" {
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+  ipv4_netmask_length = 28
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "Name" = "Workspace"
+    "Goal" = "Study"
+  }
+}
+
+
+
+# CRIAR EC2
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-830c94e3"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "ExampleAppServerInstance"
+#   }
+# }
