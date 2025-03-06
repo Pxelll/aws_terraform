ebfe5a8bc18af60097361458fd4dbbbe80f51901
Thiago Adam Camargo <thiagohsm2009@hotmail.com>
2025-03-05 23:41:57 -0300

    security groups created

--- network.tf
+++ network.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "vpc_workspace" {
-  cidr_block = "10.0.0.0/28"
-  instance_tenancy = "default"
-  enable_dns_support = true
+  cidr_block           = "10.0.0.0/28"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
@@ -16,13 +16,13 @@ resource "aws_internet_gateway" "Internet_Access" {
   tags = {
     "Name" = "Internet Access"
   }
-  
+
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id = aws_vpc.vpc_workspace.id
-  cidr_block = "10.0.0.0/29"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.vpc_workspace.id
+  cidr_block              = "10.0.0.0/29"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -31,9 +31,9 @@ resource "aws_subnet" "public_subnet" {
 }
 
 resource "aws_subnet" "private_subnet" {
-  vpc_id = aws_vpc.vpc_workspace.id
-  cidr_block = "10.0.0.0/29"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.vpc_workspace.id
+  cidr_block              = "10.0.0.0/29"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = false
   tags = {
     "Network" = "Closed"
@@ -48,11 +48,11 @@ resource "aws_route_table" "public_route_table" {
     gateway_id = aws_internet_gateway.Internet_Access.id
   }
   tags = {
-    "Name" = "Public route table for internet access" 
+    "Name" = "Public route table for internet access"
   }
 }
 
 resource "aws_route_table_association" "public_route_table_association" {
-  subnet_id = aws_subnet.public_subnet.id
+  subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
---
+++ security_groups.tf
@@ -0,0 +1,55 @@
+# Security groups: public_sg, private_sg
+
+resource "aws_security_group" "public_sg" {
+  vpc_id      = aws_vpc.vpc_workspace.id
+  name        = "http_ssh"
+  description = "Allow inbound traffic from the internet"
+}
+
+resource "aws_security_group" "private_sg" {
+  vpc_id      = aws_vpc.vpc_workspace.id
+  name        = "private"
+  description = "Do not allow inbound traffic from the internet"
+}
+
+# Ingress rules for public_sg
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_public" {
+  security_group_id = aws_security_group.public_sg.id
+  cidr_ipv4         = aws_vpc.vpc_workspace.cidr_block
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_public" {
+  security_group_id = aws_security_group.public_sg.id
+  cidr_ipv4         = aws_vpc.vpc_workspace.cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "anything_can_go_out_public" {
+  security_group_id = aws_security_group.public_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  #cidr_ipv4         = aws_security_group.private_sg.id 
+  ip_protocol = "-1"
+}
+
+# Ingress rules for private_sg
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_private" {
+  security_group_id = aws_security_group.private_sg.id
+  cidr_ipv4         = aws_vpc.vpc_workspace.cidr_block
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "anything_can_go_out_private" {
+  security_group_id = aws_security_group.private_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  #cidr_ipv4         = aws_security_group.private_sg.id 
+  ip_protocol = "-1"
+}
