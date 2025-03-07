c08182e41d6810b2ceb01fb84030dcbd33012ee4
Thiago Adam Camargo <thiagohsm2009@hotmail.com>
2025-03-06 23:36:50 -0300

    fix for network

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "vpc_workspace" {
-  cidr_block           = "10.0.0.0/28"
+  cidr_block           = "10.0.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -21,21 +21,23 @@ resource "aws_internet_gateway" "Internet_Access" {
 
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.vpc_workspace.id
-  cidr_block              = "10.0.0.0/29"
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
+    "Name"    = "Public Subnet"
     "Network" = "Open"
   }
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = aws_vpc.vpc_workspace.id
-  cidr_block              = "10.0.0.0/29"
+  cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = false
   tags = {
+    "Name"    = "Private Subnet"
     "Network" = "Closed"
   }
 }
