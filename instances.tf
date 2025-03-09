22728a5ee99727b8540e91bda1dab6a75ad696d4
Thiago Adam Camargo <thiagohsm2009@hotmail.com>
2025-03-09 18:15:59 -0300

    changes in user_data to use variables

--- instances.tf
+++ instances.tf
@@ -19,23 +19,23 @@ resource "local_sensitive_file" "aws_key_pem" {
 
 resource "aws_instance" "public_ec2" {
   instance_type          = "t2.micro"
-  ami                    = "ami-04b4f1a9cf54c11d0"
+  ami                    = var.ami_image
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.public_sg.id]
   key_name               = aws_key_pair.ec2_key.key_name
 
   user_data = <<-EOF
               #!/bin/bash
-              sudo useradd -m adam
-              echo "adam:noble6" | sudo chpasswd
-              sudo usermod -aG wheel adam
-              mkdir -p /home/adam/.ssh
-              cp /home/ubuntu/.ssh/authorized_keys /home/adam/.ssh/authorized_keys
-              chown -R adam:adam /home/adam/.ssh
-              chmod 700 /home/adam/.ssh
-              chmod 600 /home/adam/.ssh/authorized_keys
-              sudo chsh -s /bin/bash adam
-              sudo usermod -aG sudo adam
+              sudo useradd -m ${var.user}
+              echo "${var.user}:noble6" | sudo chpasswd
+              sudo usermod -aG wheel ${var.user}
+              mkdir -p /home/${var.user}/.ssh
+              cp /home/ec2-user/.ssh/authorized_keys /home/${var.user}/.ssh/authorized_keys
+              chown -R ${var.user}:${var.user} /home/${var.user}/.ssh
+              chmod 700 /home/${var.user}/.ssh
+              chmod 600 /home/${var.user}/.ssh/authorized_keys
+              sudo chsh -s /bin/bash ${var.user}
+              sudo usermod -aG sudo ${var.user}
               EOF
 
   tags = {
@@ -46,20 +46,23 @@ resource "aws_instance" "public_ec2" {
 
 resource "aws_instance" "private_ec2" {
   instance_type          = "t2.micro"
-  ami                    = "ami-04b4f1a9cf54c11d0"
+  ami                    = var.ami_image
   subnet_id              = aws_subnet.private_subnet.id
   vpc_security_group_ids = [aws_security_group.private_sg.id]
   key_name               = aws_key_pair.ec2_key.key_name
 
   user_data = <<-EOF
               #!/bin/bash
-              sudo useradd -m adam
-              echo "adam:noble6" | sudo chpasswd
-              sudo usermod -aG wheel adam
-              sudo chsh -s /bin/bash adam
-              sudo usermod -aG sudo adam
-              sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-              sudo systemctl restart sshd
+              sudo useradd -m ${var.user}
+              echo "${var.user}:noble6" | sudo chpasswd
+              sudo usermod -aG wheel ${var.user}
+              mkdir -p /home/${var.user}/.ssh
+              cp /home/ec2-user/.ssh/authorized_keys /home/${var.user}/.ssh/authorized_keys
+              chown -R ${var.user}:${var.user} /home/${var.user}/.ssh
+              chmod 700 /home/${var.user}/.ssh
+              chmod 600 /home/${var.user}/.ssh/authorized_keys
+              sudo chsh -s /bin/bash ${var.user}
+              sudo usermod -aG sudo ${var.user}
               EOF
 
   tags = {
